493fbab48aa1195cd2ccbc2705151a98197ce195
HeorhiiS <HeorhiiS@UA-HEORHIISH>
2023-11-30 19:18:06 +0200

    Add project files.

---
+++ QALight_Task_1/Mobile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QALight_Task_1
+{
+    public class Mobile
+    {
+        private string _phoneModel = "Pixel 7";
+        private string _osName = "Android";
+        private int _osVersion = 14;
+       
+        public void ShowPhoneName()
+        {
+            Console.WriteLine($"Phone name is {_phoneModel}");
+        }
+
+        public void ShowOsName()
+        {
+            Console.WriteLine($"OS name is {_osName}");
+        }
+
+        public void ShowOsVerison()
+        {
+            Console.WriteLine($"OS version is {_osVersion}");
+        }
+
+        public int GetOsVersion()
+        {
+            return _osVersion;
+        }
+
+    }
+}
---
+++ QALight_Task_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace QALight_Task_1
+{
+    class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            var mobileAndroid = new Mobile();
+            
+            //Call mobile`s methods
+            mobileAndroid.ShowPhoneName();
+            mobileAndroid.ShowOsName();
+
+            //If else block to choose needed method
+            Console.WriteLine("Type 1 if you want to get OS version value from void class.\nType 2 if you want to get OS version value from int class: ");
+            var choice = Convert.ToInt32(Console.ReadLine());
+            
+            if (choice == 1)
+            {
+                mobileAndroid.ShowOsVerison();
+            }
+
+            else
+            {
+                var osVersion = mobileAndroid.GetOsVersion();
+                Console.WriteLine($"OS version value from int method: {osVersion}");
+            }
+
+
+        }
+    }
+}
