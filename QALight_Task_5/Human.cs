f2f7478d613a2ef1670d4bfe75b1549c11182f7a
Heorhii Sashniev <gsashnev@gmail.com>
2023-12-30 21:00:10 +0200

    Homework 5.

---
+++ QALight_Task_5/Human.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace QALight_Task_5
+{
+    public class Human
+    {
+        public string Name { get; set; }
+
+    
+
+        public void GetName(Human human)
+        {
+            {
+                try
+                {
+                    Console.WriteLine($"Human name is {human.Name}");
+                }
+                catch (NullReferenceException)
+                {
+                    Console.WriteLine("Human name cannot be null!");
+                }
+            }
+        }
+    }
+}
---
+++ QALight_Task_5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace QALight_Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1
+            int IntVariable = 0;
+            Console.WriteLine("All values < 10 using while loop:");
+            while (IntVariable < 10)
+                {
+                Console.WriteLine(IntVariable++);
+            }
+
+            //Task 2
+            int IntVariableTen = 10;
+            while (IntVariableTen < 10)
+            {
+                Console.WriteLine(IntVariableTen++);
+            }
+
+            //Task 3
+            int IntVariableDoWhile = 0;
+            Console.WriteLine("All values < 10 using do-while loop:");
+            do
+            {
+                Console.WriteLine(IntVariableDoWhile++);
+            }
+            while (IntVariableDoWhile < 10);
+
+            //Task 4
+            int IntVariableTenDoWhile = 10;
+            Console.WriteLine($"All values < 10 using do-while loop with IntVariableTenDoWhile = {IntVariableTenDoWhile}:");
+            do
+            {
+                Console.WriteLine(IntVariableTenDoWhile++);
+            }
+            while (IntVariableTenDoWhile < 10);
+
+            //Task 5
+            Human human = new Human();
+            human.GetName(null);
+        }
+    }
+}
