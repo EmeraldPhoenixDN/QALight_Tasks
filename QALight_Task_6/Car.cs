b1bbb991da85b8ada9430c9b0e97ab17ebcb1cb3
Heorhii Sashniev <gsashnev@gmail.com>
2024-01-08 14:07:36 +0200

    Homework 6

---
+++ QALight_Task_6/Car.cs
@@ -0,0 +1,8 @@
+﻿namespace QALight_Task_6
+{
+    public class Car
+    {
+        public int YearOfManufacture { get; set; }
+        public string NameOfCar { get; set; }
+    }
+}
---
+++ QALight_Task_6/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QALight_Task_6
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Task 1
+            List<int> anyType = new List<int> {1, 2, 3, 4, 5};
+            GetList(anyType);
+            GetList(null);
+
+
+            //Task 2
+            List<Car> cars = new List<Car>
+        {
+            new Car { YearOfManufacture = 1997, NameOfCar = "BMW"},
+            new Car { YearOfManufacture = 1989, NameOfCar = "Audi"},
+            new Car { YearOfManufacture = 2006, NameOfCar = "Toyota"},
+            new Car { YearOfManufacture = 2023, NameOfCar = "Ford" },
+            new Car { YearOfManufacture = 2000, NameOfCar = "Tavria"},
+        };
+            List<Car> oldCars = cars.Where(car => car.YearOfManufacture <= 2000).ToList();
+            List<Car> newCars = cars.Where(car => car.YearOfManufacture > 2000).ToList();
+            List<string> oldCarNames = cars.Where(car => car.YearOfManufacture <= 2000).Select(car => car.NameOfCar).ToList();
+
+            Console.WriteLine("\nOld cars` list:");
+            foreach (var car in oldCars)
+            {
+                Console.WriteLine($"{car.YearOfManufacture} was manufactured in {car.NameOfCar}");
+            }
+
+            Console.WriteLine("\nNew cars` list:");
+            foreach (var car in newCars)
+            {
+                Console.WriteLine($"{car.YearOfManufacture} was manufactured in {car.NameOfCar}");
+            }
+
+            Console.WriteLine("\nOld car names` list:");
+            foreach (var name in oldCarNames)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+
+
+        //Task 1
+        public static void GetList(List <int> anyType)
+        {
+            try
+            {
+                Console.WriteLine($"5th list element is {anyType[5]}");
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("There is a null reference exception.");
+            }
+            catch (IndexOutOfRangeException) 
+            {
+                Console.WriteLine("There is an index out of range exception.");
+            }
+        }
+    }
+}
